c3cc405ed53d66c1aeb275726e1facfcde236072
Heta <hetahartzell@protonmail.com>
2024-11-08 14:07:08 +0200

    Station model edited

--- Sprint2/server/models/stationModel.js
+++ Sprint2/server/models/stationModel.js
@@ -30,7 +30,14 @@ function addOne(stationData) {
   const { name, location, coordinates, connectors, availability, provider } =
     stationData;
 
-  if (!name || !location || !coordinates || !connectors || !availability || !provider) {
+  if (
+    !name ||
+    !location ||
+    !coordinates ||
+    !connectors ||
+    !availability ||
+    !provider
+  ) {
     return false;
   }
 
@@ -49,7 +56,7 @@ function findById(id) {
   return item || false;
 }
 
-function UpdateOneById(id, updatedData) {
+function updateOneById(id, updatedData) {
   const station = findById(id);
   if (station) {
     Object.assign(station, updatedData);
@@ -62,19 +69,49 @@ function deleteOneById(id) {
   const item = findById(id);
   if (item) {
     const initialLength = stationArray.length;
-    stationArray = stationArray.filter(
-      (station) => station.id !== ParseInt(id)
-    );
+    stationArray = stationArray.filter((station) => station.id !== Number(id));
     return stationArray.length < initialLength;
   }
   return false;
+}
+
+if (require.main === module) {
+  // Add station
+  let result = addOne({
+    name: 'Station X',
+    location: 'Helsinki',
+    coordinates: {
+      latitude: 60.1699,
+      longitude: 24.9355,
+    },
+    connectors: ['CHAdeMO', 'CCS'],
+    availability: 'Occupied',
+    provider: 'Virta',
+  });
+  console.log('addOne called: ', result);
+
+  // Get all stations
+  const allStations = getAll();
+  console.log('getAll called: ', allStations);
+
+  // Find by id
+  const station = findById(1);
+  console.log('findById called: ', station);
+
+  // Update by id
+  const updatedStation = updateOneById(1, { name: 'Station HEBU' });
+  console.log('updateOneById called: ', updatedStation);
+
+  // Delete by id
+  const deletedStation = deleteOneById(1);
+  console.log('deleteOneById called: ', deletedStation);
 }
 
 const Station = {
   getAll,
   addOne,
   findById,
-  UpdateOneById,
+  updateOneById,
   deleteOneById,
 };
 
