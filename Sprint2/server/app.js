419ca00a3781ee8219f664c7c75e82e96899d6f7
Heta <hetahartzell@protonmail.com>
2024-11-08 14:59:51 +0200

    Logger added to middleware

--- Sprint2/server/app.js
+++ Sprint2/server/app.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const stationRoutes = require('./routes/stationRoutes');
 const userRoutes = require('./routes/userRoutes');
+const logger = require('./middleware/logger');
 
 app.use(express.json());
+app.use(logger);
 
 app.use('/stations', stationRoutes);
 app.use('/users', userRoutes);
