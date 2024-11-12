d4713747497dd214185912181c361362a7057cde
Alvaade <ade.aiho2@gmail.com>
2024-11-12 14:49:17 +0200

    configured the leaflet api map

--- Sprint2/client/src/App.jsx
+++ Sprint2/client/src/App.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Hero from "./components/Hero";
 import Login from "./components/Login";
+import Map from "./components/Map";
+import stations from "../../server/mock-data/ev_stations_mock_data.json";
 
 /* Placeholder code for testing */
 function App() {
   return (
     <>
       <div className="max-w-screen-2xl m-auto">
-        <Login />
+        <Map stations={stations} />
       </div>
     </>
   );
