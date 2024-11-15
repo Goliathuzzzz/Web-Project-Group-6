602ea57be97f8eafc362cca79ecf722ac33b2652
Jonne Roponen <Jonne.Roponen@metropolia.fi>
2024-11-15 20:49:16 +0200

    make small adjustment to style

--- Sprint2/client/src/components/callToActionComponent/CallToAction.jsx
+++ Sprint2/client/src/components/callToActionComponent/CallToAction.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function CallToAction() {
   return (
-    <div className="flex justify-center w-full rounded-md">
+    <div className="flex justify-center w-full rounded-md pb-4">
       <div className="flex flex-col items-center justify-center bg-ctaBg bg-cover w-[70%]">
-        <h2 className="text-electricBlue text-2xl tracking-wider w-full text-center p-2 mt-10 font-Orbitron">
+        <h2 className="text-electricBlue text-2xl tracking-wider w-full text-center p-2 mt-10 font-Orbitron font-bold">
           Map Out Your Next Charge in Seconds
         </h2>
         <div className="text-white text-center text-lg my-2 font-Roboto">
