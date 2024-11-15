8bdf6ffaf271aaf9982d3b00a55986bc10c6b3e9
Jonne Roponen <Jonne.Roponen@metropolia.fi>
2024-11-15 20:58:51 +0200

    change images, overall style, responsiviness

--- Sprint2/client/src/components/ReviewComponents/Review.jsx
+++ Sprint2/client/src/components/ReviewComponents/Review.jsx
@@ -3,13 +3,13 @@ import star from "../../assets/images/star.png";
 
 function Review({ img, name, text, stars }) {
   return (
-    <div className="flex bg-mediumBlue rounded-lg p-2 mx-6 my-3">
+    <div className="flex bg-mediumBlue rounded-lg p-2 pl-4 py-3 mx-6 my-1.5">
       <div className="flex items-center">
         <img src={img} alt={name} className="w-40 rounded-full" />
       </div>
       <div className="flex flex-col ml-4">
         <p className="text-white font-Roboto text-xs">
-          {text} - {name}
+          {text}  {name}
         </p>
         <div className="flex mt-2 justify-end">
           {Array.from({ length: stars }, (_, index) => {
