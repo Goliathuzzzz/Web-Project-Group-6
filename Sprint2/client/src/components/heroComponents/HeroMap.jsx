a19b72b83886bdbb25fc68c5a8ce0a4de02fc216
Jonne Roponen <Jonne.Roponen@metropolia.fi>
2024-11-15 20:52:17 +0200

    add map, search bar, open map button with mobile responsiviness

--- Sprint2/client/src/components/heroComponents/HeroMap.jsx
+++ Sprint2/client/src/components/heroComponents/HeroMap.jsx
@@ -1,11 +1,34 @@
 import React from "react";
+import Map from "../../assets/images/map_mini.png";
+import Search from "../../assets/images/search.png";
 
 function HeroMap() {
   return (
     <>
       {/* Map container */}
-      <div className="flex bg-slate-400 h-80 md:w-[40%]">
-        <h2>Map Placeholder container</h2>
+      <div className="relative h-80 md:w-[40%]">
+        <img src={Map} alt="mini-map" className="object-cover w-full h-full" />
+        {/* Small search bar in the top left corner */}
+        <div className="absolute top-4 left-4 w-7/12 flex flex-col md:map-buttons-size:flex-row map-buttons-size:items-center map-buttons-size:space-x-4">
+          <div className="relative w-full">
+            <input
+              type="text"
+              name="query"
+              placeholder="Search"
+              className="w-full py-1 pl-10 pr-4 text-white rounded-full bg-searchBarBg focus:outline-none focus:ring-2 focus:ring-searchBarSelected shadow-inner-lg font-Roboto"
+            />
+            <img
+              src={Search}
+              alt="Search Icon"
+              className="absolute left-3 top-1.5 h-5 w-5 text-gray-400 font-Roboto"
+            />
+          </div>
+          <button
+            className="mt-4 map-buttons-size:mt-0 w-fit py-1.5 px-7 text-sm text-white rounded-full bg-gradient-to-b from-darkerBlue to-darkBlue focus:outline-none focus:ring-2 focus:ring-searchBarSelected font-Roboto whitespace-nowrap"
+          >
+            Open Map
+          </button>
+        </div>
       </div>
     </>
   );
