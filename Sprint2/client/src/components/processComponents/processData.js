835dab0a06022e525ca31e807dc25a71238bc59f
Jonne Roponen <Jonne.Roponen@metropolia.fi>
2024-11-15 20:56:56 +0200

    change images, text size, responsiviness, and style

--- Sprint2/client/src/components/processComponents/processData.js
+++ Sprint2/client/src/components/processComponents/processData.js
@@ -1,7 +1,7 @@
-import locate_pic from '../../assets/images/locate_pic.png';
-import charge_pic from '../../assets/images/charge_pic.png';
-import favorite_pic from '../../assets/images/favorite_pic.png';
-import navigate_pic from '../../assets/images/navigate_pic.png';
+import locate_pic from '../../assets/images/locate_pic2.png';
+import charge_pic from '../../assets/images/charge_pic2.png';
+import favorite_pic from '../../assets/images/favorite_pic2.png';
+import navigate_pic from '../../assets/images/navigate_pic2.png';
 
 export const processData = [
     { id: 1, image: locate_pic, text: "Find nearby EV charging stations on our map." },
