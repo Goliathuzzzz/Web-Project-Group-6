58ae5d2f794e70b76eb8d5547d2fb22797460afa
Jonne Roponen <Jonne.Roponen@metropolia.fi>
2024-11-16 15:16:16 +0200

    add styling

--- Sprint2/client/src/components/Login.jsx
+++ Sprint2/client/src/components/Login.jsx
@@ -1,13 +1,12 @@
 import React, { useState } from 'react';
 import google from '../assets/images/google_logo.png';
 import image from '../assets/images/login_page.png';
+import { Link } from 'react-router-dom';
 
 function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [rememberMe, setRememberMe] = useState(false);
-
-  //google login
 
   const handleLogin = () => {
     if (username && password) {
@@ -20,29 +19,30 @@ function Login() {
 
   return (
     <div className="flex items-center justify-center h-screen w-screen">
-      <div className="flex flex-col bg-white max-w-[450px] min-w-[300px] lg:w-1/2 xl:w-1/2 sm:w-1/2 justify-center items-center h-1/2 w-1/4 font-Roboto">
-        <button className="flex items-center justify-center border p-2 mb-2 w-3/4 rounded-sm border-borderBlue">
+      <div className="flex flex-col bg-white max-w-[450px] min-w-[300px] lg:w-1/2 xl:w-1/2 sm:w-1/2 justify-center items-center h-2/3 w-1/4 font-Roboto mb-36 rounded-tl-sm rounded-bl-sm">
+        <button className="flex items-center justify-center border p-2 mb-2.5 w-3/4 rounded-sm border-borderBlue">
           <img src={google} alt="Google Logo" className="w-6 h-6 mr-2" />
           <span className="text-gray-700 font-medium text-xs">
             Continue with Google
           </span>
         </button>
-        <p className="flex mb-2 font-bold">Or</p>
+        <p className="flex mb-2.5 font-bold">Or</p>
         <form
           className="w-full flex flex-col items-center"
           onSubmit={(e) => e.preventDefault()}
         >
           <input
-            className="flex w-3/4 border mb-2 rounded-sm bg-inputGrey text-xs p-1 border-borderBlue pl-2"
+            className="flex w-3/4 border mb-2.5 rounded-sm bg-inputGrey text-xs p-1 border-borderBlue pl-2 pt-2 pb-2"
             placeholder="Username"
             onChange={(e) => setUsername(e.target.value)}
           ></input>
           <input
-            className="flex w-3/4 border mb-2 rounded-sm bg-inputGrey text-xs p-1 border-borderBlue "
+            className="flex w-3/4 border mb-2.5 rounded-sm bg-inputGrey text-xs p-1 border-borderBlue pl-2 pt-2 pb-2"
             placeholder="Password"
+            type="password" // For security, use type="password"
             onChange={(e) => setPassword(e.target.value)}
           ></input>
-          <div className="w-3/4 flex flex-row justify-start mb-2 items-center">
+          <div className="w-3/4 flex flex-row justify-start mb-2.5 items-center">
             <input
               type="checkbox"
               className="appearance-none h-3 w-3 rounded-sm border border-borderBlue checked:bg-borderBlue checked:after:content-['✓'] checked:after:text-xs flex items-center justify-center focus:ring-2 focus:ring-offset-1"
@@ -51,22 +51,29 @@ function Login() {
               onChange={() => setRememberMe(!rememberMe)}
             ></input>
             <label htmlFor="remember-me" className="pl-2 font-bold text-xs p-1">
-              Remember me
+              Remember Me
             </label>
           </div>
           <button
             type="button"
             onClick={handleLogin}
-            className="flex w-3/4 justify-center p-2 rounded-3xl font-bold bg-gradient-to-r from-eGreen to-eGreenDark"
+            className="flex w-3/4 justify-center p-2 rounded-3xl font-bold bg-gradient-to-r from-eGreen to-eGreenDark hover:bg-darkGreen hover:text-white transition-all duration-500"
           >
             Login
           </button>
         </form>
+        <p className="mt-4 text-xs">
+          Don't have an account?{' '}
+          <Link to="/register" className="text-blue-500 font-bold hover:text-blue-900">
+            Sign up
+          </Link>
+        </p>
       </div>
-      <div className="h-screen flex items-center justify-center sm:">
-        <img src={image} alt="placeholder" className="h-1/2 sm:block hidden" />
+      <div className="h-screen flex items-center justify-center mb-36">
+        <img src={image} alt="placeholder" className="h-2/3 sm:block hidden rounded-tr-sm rounded-br-sm" />
       </div>
     </div>
   );
 }
+
 export default Login;
