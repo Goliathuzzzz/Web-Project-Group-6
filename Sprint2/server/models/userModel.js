cde97a1670253f11cef70cdea62ccfded67f4800
Heta <hetahartzell@protonmail.com>
2024-11-16 19:40:04 +0200

    Merged heta branch

--- Sprint2/server/models/userModel.js
+++ Sprint2/server/models/userModel.js
@@ -1,64 +1,23 @@
-// Data model for user
-// {
-//     "username": "hetahar",
-//     "password": "123456",
-//     "email":" : "heppulipeppuli@proton.me"
-// }
+const mongoose = require('mongoose');
 
-let userArray = [];
-let nextId = 1;
+const Schema = mongoose.Schema;
 
-function getAll() {
-  return userArray;
-}
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    }
+  },
+  { timestamps: true }
+);
 
-function addOne(userData) {
-  const { username, password, email } = userData;
-
-  if (!username || !password || !email) {
-    return false;
-  }
-
-  const newUser = {
-    id: nextId++,
-    ...userData,
-  };
-
-  userArray.push(newUser);
-  return newUser;
-}
-
-function findById(id) {
-  const numericId = parseInt(id);
-  const item = userArray.find((user) => user.id === numericId);
-  return item || false;
-}
-
-function updateOneById(id, updatedData) {
-  const user = findById(id);
-  if (user) {
-    Object.assign(user, updatedData);
-    return user;
-  }
-  return false;
-}
-
-function deleteOneById(id) {
-  const user = findById(id);
-  if (user) {
-    const initialLength = userArray.length;
-    userArray = userArray.filter((user) => user.id !== Number(id));
-    return userArray.length < initialLength;
-  }
-  return false;
-}
-
-const User = {
-    getAll,
-    addOne,
-    findById,
-    updateOneById,
-    deleteOneById,
-};
-
-module.exports = User;
+module.exports = mongoose.model('User', userSchema);
