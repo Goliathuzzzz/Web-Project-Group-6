d500dc3edce5c2e2dd94d2ae12822a38427e1e9d
Jonne Roponen <Jonne.Roponen@metropolia.fi>
2024-12-04 19:32:48 +0200

    add style

--- Sprint3/client/src/components/contactComponents/ContactUs.jsx
+++ Sprint3/client/src/components/contactComponents/ContactUs.jsx
@@ -1,12 +1,12 @@
-import { useState, useEffect } from 'react';
-import contactData from './data';
-import carImage from '../../assets/images/car-w-charger.jpg';
+import { useState, useEffect } from "react";
+import contactData from "./data";
+import carImage from "../../assets/images/car-w-charger.jpg";
 
 const ContactUs = () => {
   const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    content: '',
+    name: "",
+    email: "",
+    content: "",
   });
 
   const handleChange = (e) => {
@@ -17,7 +17,7 @@ const ContactUs = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!formData.name || !formData.email || !formData.content) {
-      alert('Please fill in all fields');
+      alert("Please fill in all fields");
       return;
     }
     const newContactForm = {
@@ -27,30 +27,30 @@ const ContactUs = () => {
     };
     addContactForm(newContactForm);
     //reset form
-    setFormData({ name: '', email: '', content: '' });
+    setFormData({ name: "", email: "", content: "" });
   };
 
   const addContactForm = async (newContactForm) => {
     try {
-      const response = await fetch('/api/contact', {
-        method: 'POST',
+      const response = await fetch("/api/contact", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(newContactForm),
       });
       if (response.ok) {
-        console.log('Form data sent successfully');
+        console.log("Form data sent successfully");
       } else {
-        alert('Failed to send form data');
+        alert("Failed to send form data");
       }
     } catch (error) {
-      console.error('Error:', error);
+      console.error("Error:", error);
     }
   };
 
   return (
-    <div className="flex items-center justify-center mb-16 mt-8">
+    <div className="flex items-center justify-center mb-36 mt-28">
       <div className="font-Roboto">
         <h1 className="text-3xl font-bold text-eGreen mb-4 font-Orbitron ">
           Contact Us
@@ -62,17 +62,17 @@ const ContactUs = () => {
         </div>
         <div className="flex flex-row items-center relative justify-center">
           {/* Contact info section */}
-          <div className="bg-white p-4 rounded-xl w-64 h-64 justify-center absolute -translate-x-1/2 left-[-20px] hidden sm:block">
+          <div className="bg-white p-4 rounded-xl w-64 h-64 justify-center absolute -translate-x-1/2 left-10 hidden contact:block">
             <div>
               <h2 className="font-bold">Email Us</h2>
-              <p className="mb-4">
+              <p className="mb-4 text-electricBlue hover:underline">
                 <a href={`mailto:${contactData.email}`}>{contactData.email}</a>
               </p>
             </div>
 
             <div>
               <h2 className="font-bold">Phone Support</h2>
-              <p className="mb-4">
+              <p className="mb-4 text-electricBlue hover:underline">
                 <a href={`tel:${contactData.phone}`}>{contactData.phone}</a>
               </p>
             </div>
@@ -87,17 +87,17 @@ const ContactUs = () => {
             <img
               src={carImage}
               alt="Car with charger"
-              className="h-[355px] rounded-l-xl"
+              className="h-[450px] rounded-l-xl hidden contact-form:block xs:hidden w-80"
             />
           </div>
 
-          {/* Contact for section */}
-          <div className="bg-white rounded-r-xl h-[355px]">
+          {/* Contact form section */}
+          <div className="bg-white rounded-xl w-80 contact-form:rounded-none contact-form:rounded-r-xl min-h-[450px] font-Roboto">
             <form onSubmit={handleSubmit} className="p-4 ">
               <h2 className="font-bold mb-4">Contact Form</h2>
 
               {/* Name Field */}
-              <div>
+              <div className="font-Roboto">
                 <label>Name: </label>
                 <input
                   type="text"
@@ -107,12 +107,12 @@ const ContactUs = () => {
                   onChange={handleChange}
                   placeholder="Your name"
                   required
-                  className="bg-lilac bg-opacity-50 w-full rounded-lg text-xs p-1 text:pl-2 placeholder:text-xs mb-2"
+                  className="bg-lilac bg-opacity-50 w-full rounded-lg text-xs p-2 text:pl-2 placeholder-gray-500 placeholder:text-xs mb-3"
                 />
               </div>
 
               {/* Email Field */}
-              <div>
+              <div className="font-Roboto">
                 <label>Email: </label>
                 <input
                   type="email"
@@ -122,12 +122,12 @@ const ContactUs = () => {
                   onChange={handleChange}
                   placeholder="Your email"
                   required
-                  className="bg-lilac bg-opacity-50 w-full rounded-lg text-xs p-1 text:pl-2 placeholder:text-xs mb-2"
+                  className="bg-lilac bg-opacity-50 w-full rounded-lg text-xs p-2 text:pl-2 placeholder-gray-500 placeholder:text-xs mb-3"
                 />
               </div>
 
               {/* Content Field */}
-              <div>
+              <div className="font-Roboto">
                 <label>Content:</label>
                 <textarea
                   id="content"
@@ -137,14 +137,14 @@ const ContactUs = () => {
                   rows="4"
                   placeholder="Your message"
                   required
-                  className="bg-lilac bg-opacity-50 w-full rounded-lg text-xs p-1 text:pl-2 placeholder:text-xs mb-2"
+                  className="bg-lilac bg-opacity-50 w-full rounded-lg text-xs p-2 placeholder-gray-500 placeholder:text-xs mb-3 min-h-[34px] max-h-[167px] overflow-y-auto"
                 ></textarea>
               </div>
 
               {/* Submit Button */}
               <button
                 type="submit"
-                className="p-2 text-sm rounded-3xl font-bold bg-gradient-to-r from-eGreen to-eGreenDark hover:bg-darkGreen hover:text-white transition-all duration-500"
+                className="p-2 px-5 text-sm rounded-3xl font-bold font-Roboto bg-gradient-to-r from-eGreen to-eGreenDark hover:bg-darkGreen hover:text-white transition-all duration-500"
               >
                 Submit
               </button>
@@ -153,17 +153,17 @@ const ContactUs = () => {
         </div>
 
         {/* Contact info for small screens */}
-        <div className="text-white mt-4 rounded-xl w-64 h-64justify-center hidden sm:hidden xs:block">
+        <div className="text-white mt-4 rounded-xl w-64 h-64 justify-center contact:hidden xs:block">
           <div>
             <h2 className="font-bold">Email Us</h2>
-            <p className="mb-4">
+            <p className="mb-4 text-blue-300 hover:underline">
               <a href={`mailto:${contactData.email}`}>{contactData.email}</a>
             </p>
           </div>
 
           <div>
             <h2 className="font-bold">Phone Support</h2>
-            <p className="mb-4">
+            <p className="mb-4 text-blue-300 hover:underline">
               <a href={`tel:${contactData.phone}`}>{contactData.phone}</a>
             </p>
           </div>
