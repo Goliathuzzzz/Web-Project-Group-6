a2dd63fe0a8d38a8adfdddb111977bf88c4dc601
Goliathuzzzz <mlaakkonen@live.com>
2024-12-07 13:52:48 +0200

    Added custom filtering to getById function

--- Sprint3/server/routes/ocmStationRoutes.js
+++ Sprint3/server/routes/ocmStationRoutes.js
@@ -112,8 +112,71 @@ router.getById('/:id', async (req, res) => {
           'X-API-Key': process.env.OPEN_CHARGE_MAP_API_KEY,
         },
       });
-    const data = response.data;
-    console.log(data);
+    const formattedData = response.data
+      .filter((item) =>
+        item.Connections?.some(
+          (
+            connection //filtering out the connection types that are not useful for the project
+          ) =>
+            [2, 25, 27, 28, 30, 32, 33, 1036].includes(
+              connection.ConnectionTypeID
+            ) //30, 27 tesla; 32 type1; 33 type2; 2 chademo; 25 type2 socket only; 28 type F slow charge; 1036 type 2 tethered
+        )
+      )
+      .map((item) => ({
+        id: item.ID || 'N/A',
+        location: {
+          title: item.AddressInfo?.Title || 'N/A',
+          addressLine1: item.AddressInfo?.AddressLine1 || 'N/A',
+          town: item.AddressInfo?.Town || 'N/A',
+          postcode: item.AddressInfo?.Postcode || 'N/A',
+          latitude: item.AddressInfo?.Latitude || 'N/A',
+          longitude: item.AddressInfo?.Longitude || 'N/A',
+        },
+        usageType: {
+          isPayAtLocation: item.UsageType?.IsPayAtLocation || false,
+          isMembershipRequired: item.UsageType?.IsMembershipRequired || false,
+          isAccessKeyRequired: item.UsageType?.IsAccessKeyRequired || false,
+          isPublic: item.UsageType?.Title || false,
+        },
+        statusType: {
+          isOperational: item.StatusType?.IsOperational || false,
+          isUserSelectable: item.StatusType?.IsUserSelectable || false,
+        },
+        isRecentlyVerified: item.IsRecentlyVerified || false,
+        dateLastVerified: item.DateLastVerified || 'N/A',
+        usageCost: item.UsageCost || 'N/A',
+        connections:
+          item.Connections?.map((connection) => ({
+            id: connection.ID || 'N/A',
+            connectionTypeID: connection.ConnectionTypeID || 'N/A',
+            powerKW: connection.PowerKW || 'N/A',
+            level: connection.LevelID || 'N/A',
+            quantity: connection.Quantity || 0,
+            isFastChargeCapable: connection.Level?.IsFastChargeCapable || false,
+            connectionType: connection.ConnectionType?.Title || 'N/A',
+            formalName: connection.ConnectionType?.FormalName || 'N/A',
+          })) || [],
+      }));
+    //for future use, when we want to filter out the stations not currently showing in map view. we can also use distance from the center point
+    // .filter((station) => {
+    //   const { latitude, longitude } = station.location;
+    //   return (
+    //     latitude >= parseFloat(south) &&
+    //     latitude <= parseFloat(north) &&
+    //     longitude >= parseFloat(west) &&
+    //     longitude <= parseFloat(east)
+    //   );
+    // });
+
+    //just for checking in console
+    const uniqueStations = [
+      ...new Map(
+        formattedData.map((item) => [item.location.title, item])
+      ).values(),
+    ];
+    console.log(`Unique Stations: ${uniqueStations.length}`);
+    res.json(formattedData);
   } catch (error) {
     console.error(error);
     res.status(500).send('Error retrieving charging point');
