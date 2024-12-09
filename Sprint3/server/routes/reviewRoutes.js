fb59b945d63dbe0511def4493e199c5d535c6337
Jonne Roponen <Jonne.Roponen@metropolia.fi>
2024-12-09 14:58:02 +0200

    remove patch route

--- Sprint3/server/routes/reviewRoutes.js
+++ Sprint3/server/routes/reviewRoutes.js
@@ -10,25 +10,14 @@ const {
   deleteReview,
 } = require('../controllers/reviewController');
 
-// GET /reviews
 router.get('/', getAllReviews);
-
-// GET /reviews/:reviewId
 router.get('/:reviewId', getReviewById);
 
 // middleware for authentication
 router.use(auth);
 
-// POST /reviews
 router.post('/', createReview);
-
-// PUT /reviews/:reviewId
 router.put('/:reviewId', updateReview);
-
-// PATCH /reviews/:reviewId
-router.patch('/:reviewId', replaceReview);
-
-// DELETE /reviews/:reviewId
 router.delete('/:reviewId', deleteReview);
 
 module.exports = router;
