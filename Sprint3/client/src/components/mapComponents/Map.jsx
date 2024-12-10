586de82d010f9ab3cd806cc4b9f4887e1c59f1c3
Goliathuzzzz <mlaakkonen@live.com>
2024-12-10 13:27:08 +0200

    Fixed zoom out issue on map

--- Sprint3/client/src/components/mapComponents/Map.jsx
+++ Sprint3/client/src/components/mapComponents/Map.jsx
@@ -84,7 +84,7 @@ const Map = ({ minimap = false }) => {
       <MapContainer
         center={INITIAL_POSITION}
         zoom={INITIAL_ZOOM}
-        minZoom={INITIAL_ZOOM}
+        minZoom={5}
         maxBounds={INITIAL_BOUNDS}
         maxBoundsViscosity={1.0}
         zoomControl={false}
