6872f3479fc951197bacb1c6705654ccc67399d5
Goliathuzzzz <mlaakkonen@live.com>
2024-12-10 14:38:06 +0200

    Added google login to registration page

--- Sprint3/client/routes/AuthProvider.jsx
+++ Sprint3/client/routes/AuthProvider.jsx
@@ -6,7 +6,9 @@ const AuthContext = createContext();
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(localStorage.getItem("user") || "");
   const [token, setToken] = useState(localStorage.getItem("site") || "");
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(
+    Boolean(token) || false
+  );
   const [errorMessage, setErrorMessage] = useState("");
   const navigate = useNavigate();
 
