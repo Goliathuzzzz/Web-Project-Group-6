c4afd15d047f461805512c849110d707a27850b1
Jonne Roponen <Jonne.Roponen@metropolia.fi>
2024-12-10 13:29:21 +0200

    adjust height for mypage

--- Sprint3/client/src/App.jsx
+++ Sprint3/client/src/App.jsx
@@ -68,13 +68,13 @@ function App() {
                   <Route
                     path="dashboard"
                     element={
-                      <>
-                        <div className="flex flex-col my-page:flex-row justify-center my-page:gap-5 mb-2">
+                      <div className='h-screen'>
+                        <div className="flex flex-col my-page:flex-row justify-center my-page:gap-5 mb-2 h-">
                           <ProfilePic />
                           <SavedStations />
                         </div>
                         <Specifications />
-                      </>
+                      </div>
                     }
                   />
                   <Route path="map" element={<Map />} />
