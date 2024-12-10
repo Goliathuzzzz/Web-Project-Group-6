62aaab11455a237a5c20a807390308e181986e7d
Goliathuzzzz <mlaakkonen@live.com>
2024-12-10 19:16:53 +0200

    Fixed myPage

--- Sprint3/client/src/components/myPageComponents/myPage.jsx
+++ Sprint3/client/src/components/myPageComponents/myPage.jsx
@@ -4,13 +4,13 @@ import Specifications from "./Specifications";
 
 function MyPage() {
   return (
-    <>
+    <div>
       <div className="flex flex-col my-page:flex-row justify-center my-page:gap-5 mb-2">
         <ProfilePic />
         <SavedStations />
       </div>
       <Specifications />
-    </>
+    </div>
   );
 }
 
